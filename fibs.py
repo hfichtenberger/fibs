66f7e8c233c6125dcd5eb30b92ce7d82e8c268ee
Hendrik Fichtenberger <hendrik.fichtenberger@tu-dortmund.de>
2016-05-15 13:45:13 +0200

    Removed all hard line width breaks

--- fibs.py
+++ fibs.py
@@ -123,9 +123,7 @@ def scheduler_start(args):
     # Get configuration
     if args.subparser_name == 'start':
         if os.path.isfile(fib_scheduler_continuation_filename):
-            print_error('Please remove the continuation file "' + fib_scheduler_continuation_filename + '" to confirm '
-                        'that you want to start over. You might also want to delete existing results and logs by '
-                        'using the reset command.')
+            print_error('Please remove the continuation file "' + fib_scheduler_continuation_filename + '" to confirm that you want to start over. You might also want to delete existing results and logs by using the reset command.')
             exit_gracefully(1)
         else:
             if not os.path.isfile(args.jobfile):
@@ -315,8 +313,7 @@ def worker_start(args):
     # Boot worker
     assert_working_dir()
     if os.path.isfile(ready_filename):
-        print_error('A worker with the ID ' + args.identifier + ' is already in running or has crashed.'
-                    'In case of the latter, consider the repair option.')
+        print_error('A worker with the ID ' + args.identifier + ' is already in running or has crashed. In case of the latter, consider the repair option.')
         sys.exit(1)
     open(ready_filename, 'w').close()
     job_filename = os.path.join(fib_job_dir, identifier)
@@ -430,30 +427,19 @@ def reset(args):
 
 def help(args):
     w = textwrap.TextWrapper(width=args.width)
-    print(w.fill('FIBS is a self-contained python script for scheduling a bunch of user-defined commands ("jobs") '
-                 'on one or more computers. There are two main modes: "scheduler" mode and "worker" mode. In scheduler '
-                 'mode, FIBS processes a job file supplied by the user (you!) and schedules all contained jobs to '
-                 'available workers. In worker mode, FIBS accepts and executes jobs deployed by the scheduler. '
-                 'Multiple workers running on the same or on different machines can connect to the same scheduler. '
-                 'The medium of communication used to distribute jobs and receive results is a shared directory, e.g. '
-                 'a subdirectory of your home directory.'))
+    print(w.fill('FIBS is a self-contained python script for scheduling a bunch of user-defined commands ("jobs") on one or more computers. There are two main modes: "scheduler" mode and "worker" mode. In scheduler mode, FIBS processes a job file supplied by the user (you!) and schedules all contained jobs to available workers. In worker mode, FIBS accepts and executes jobs deployed by the scheduler. Multiple workers running on the same or on different machines can connect to the same scheduler. The medium of communication used to distribute jobs and receive results is a shared directory, e.g. a subdirectory of your home directory.'))
     print('')
     print(w.fill('# Job file'))
     print(w.fill('The job file uses a simple text format where each line corresponds to a job. The line format is:'))
     print(w.fill('<n> <m> <cmd>'))
-    print(w.fill('This will instruct the scheduler to schedule the command <cmd> to available workers <n> times '
-                 'in batches of size <m>. For example'))
+    print(w.fill('This will instruct the scheduler to schedule the command <cmd> to available workers <n> times in batches of size <m>. For example'))
     print(w.fill('10 2 echo "Hello world!"'))
-    print(w.fill('would schedule the echo command to available workers five times, and each time a worker would '
-                 'print "Hello world!" twice. If a job fails, i.e., its exit code differs from 0, the whole batch is '
-                 'discarded and queued for schedule again.'))
-    print (w.fill('The commands may contain placeholders:'))
+    print(w.fill('would schedule the echo command to available workers five times, and each time a worker would print "Hello world!" twice. If a job fails, i.e., its exit code differs from 0, the whole batch is discarded and queued for schedule again.'))
+    print(w.fill('The commands may contain placeholders:'))
     w.subsequent_indent = '  '
     print(w.fill('- $HOST$ will be replaced by the worker\'s hostname'))
-    print(w.fill('- $INSTANCE$ will be replaced by the worker\'s identifier which is unique amongst all workers '
-                 'running on the same host'))
-    print(w.fill('- $OUTPUT$ will be replaced by the full path to a directory whose top-level files are collected by '
-                 'the scheduler after the job (batch) was successfully executed'))
+    print(w.fill('- $INSTANCE$ will be replaced by the worker\'s identifier which is unique amongst all workers running on the same host'))
+    print(w.fill('- $OUTPUT$ will be replaced by the full path to a directory whose top-level files are collected by the scheduler after the job (batch) was successfully executed'))
     w.subsequent_indent = ''
     print('')
     print(w.fill('# Basic usage'))
@@ -470,39 +456,23 @@ def help(args):
     print(w.fill('> python ' + sys.argv[0] + ' worker <id>'))
     w.subsequent_indent = ''
     print(w.fill('where <id> is a identifier which has to be unique amongst all workers running on the same host.'))
-    print(w.fill('You may start workers on machines that differ from the one running the scheduler. The only '
-                 'requirement is that the working directory must be accessible. For runtime measurements, you '
-                 'should consider using a separate machine for scheduling.'))
-    print(w.fill('More commands exist, and help texts are available through the -h / --help commandline argument.'
-                 'Example:'))
+    print(w.fill('You may start workers on machines that differ from the one running the scheduler. The only requirement is that the working directory must be accessible. For runtime measurements, you should consider using a separate machine for scheduling.'))
+    print(w.fill('More commands exist, and help texts are available through the -h / --help commandline argument. Example:'))
     w.subsequent_indent = '  '
     print(w.fill('> python ' + sys.argv[0] + ' -h scheduler'))
     print(w.fill('> python ' + sys.argv[0] + ' worker -h start'))
     w.subsequent_indent = ''
     print('')
     print(w.fill('# Directory structure'))
-    print(w.fill('Results are collected in the "' + fib_result_dir + '" directory. Logs are placed in the "'
-                 + fib_log_dir + '" directory. "' + fib_token_dir + '" is used by the workers to announce their '
-                                                                    'presence to the scheduler, "' + fib_job_dir
-                 + '" is used by the scheduler to deploy jobs the workers and "' + fib_temp_dir
-                 + '" is the worker output directory from where the scheduler moves the result files to the results '
-                   'directory after a job finished successfully, i.e., its exit code equals 0. The file "'
-                 + fib_scheduler_running_filename + '" announces that a scheduler is running in the working '
-                                                    'directory. Continuation files are named "'
-                 + fib_scheduler_continuation_filename + '".'))
+    print(w.fill('Results are collected in the "' + fib_result_dir + '" directory. Logs are placed in the "' + fib_log_dir + '" directory. "' + fib_token_dir + '" is used by the workers to announce their presence to the scheduler, "' + fib_job_dir + '" is used by the scheduler to deploy jobs the workers and "' + fib_temp_dir + '" is the worker output directory from where the scheduler moves the result files to the results directory after a job finished successfully, i.e., its exit code equals 0. The file "' + fib_scheduler_running_filename + '" announces that a scheduler is running in the working directory. Continuation files are named "' + fib_scheduler_continuation_filename + '".'))
     print('')
     print(w.fill('# Advanced commands'))
-    print(w.fill('There is a "repair" command to repair corrupted working directories, e.g. after a worker crashed '
-                 '(which will hopefully never happen!). Please make sure that no scheduler and no workers are '
-                 'running in the working directory before using it.'))
-    print(w.fill('Use the "reset" command to wipe all data in your working directory. Be aware that this will delete '
-                 'all results and logs, too! To keep you safe from harm, there is a preceding test of your '
-                 'soundness of mind before anything is touched.'))
+    print(w.fill('There is a "repair" command to repair corrupted working directories, e.g. after a worker crashed which will hopefully never happen!). Please make sure that no scheduler and no workers are running in the working directory before using it.'))
+    print(w.fill('Use the "reset" command to wipe all data in your working directory. Be aware that this will delete all results and logs, too! To keep you safe from harm, there is a preceding test of your soundness of mind before anything is touched.'))
 
 
 def startup():
-    parser = argparse.ArgumentParser(description='FIBS - a file-based scheduler',
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(description='FIBS - a file-based scheduler', formatter_class=argparse.RawDescriptionHelpFormatter)
     subparsers = parser.add_subparsers()
     # Layer 0: Scheduler command
     parser_scheduler = subparsers.add_parser('scheduler', help='scheduler mode')
@@ -513,37 +483,20 @@ def startup():
     assert isinstance(parser_scheduler_start, argparse.ArgumentParser)
     parser_scheduler_start.set_defaults(func=scheduler_start)
     parser_scheduler_start.add_argument('jobfile', help='path to job file')
-    parser_scheduler_start.add_argument('-e', '--exitworker', action='store_true',
-                                        help='scheduler will send exit signals to all workers after completing '
-                                             'job list.')
-    parser_scheduler_start.add_argument('-i', '--idletime', action='store', type=float, default=5,
-                                        help='timespan to sleep when there is nothing to to (polling interval).')
-    parser_scheduler_start.add_argument('-r', '--resign', action='store', type=int, default=5,
-                                        help='number of times a job is allowed to fail before it is removed from '
-                                             'scheduling.')
-    parser_scheduler_start.add_argument('-m', '--sendmail', action='store', type=str,
-                                        help='a mail will be sent to this address when scheduling is paused'
-                                             'or finished. Uses the "mail" command. ')
+    parser_scheduler_start.add_argument('-e', '--exitworker', action='store_true', help='scheduler will send exit signals to all workers after completing job list.')
+    parser_scheduler_start.add_argument('-i', '--idletime', action='store', type=float, default=5, help='timespan to sleep when there is nothing to to (polling interval).')
+    parser_scheduler_start.add_argument('-r', '--resign', action='store', type=int, default=5, help='number of times a job is allowed to fail before it is removed from scheduling.')
+    parser_scheduler_start.add_argument('-m', '--sendmail', action='store', type=str, help='a mail will be sent to this address when scheduling is paused or finished. Uses the "mail" command. ')
     # Layer 1: Scheduler Continue command
-    parser_scheduler_continue = subparsers_scheduler.add_parser('continue', help='continue previously paused job '
-                                                                                 'processing')
+    parser_scheduler_continue = subparsers_scheduler.add_parser('continue', help='continue previously paused job processing')
     assert isinstance(parser_scheduler_continue, argparse.ArgumentParser)
     parser_scheduler_continue.set_defaults(func=scheduler_start)
-    parser_scheduler_continue.add_argument('-e', '--exitworker', action='store_true',
-                                           help='scheduler will send exit signals to all workers after'
-                                                'completing job list.')
-    parser_scheduler_continue.add_argument('-i', '--idletime', action='store', type=float, default=5,
-                                           help='timespan to sleep when there is nothing to to (scheduling interval).')
-    parser_scheduler_continue.add_argument('-r', '--resign', action='store', type=int, default=5,
-                                           help='number of times a job is allowed to fail before it is removed from '
-                                                'scheduling.')
-    parser_scheduler_continue.add_argument('-m', '--sendmail', action='store', type=str,
-                                        help='a mail will be sent to this address when scheduling is paused'
-                                             'or finished. Uses the "mail" command. ')
+    parser_scheduler_continue.add_argument('-e', '--exitworker', action='store_true', help='scheduler will send exit signals to all workers after completing job list.')
+    parser_scheduler_continue.add_argument('-i', '--idletime', action='store', type=float, default=5, help='timespan to sleep when there is nothing to to (scheduling interval).')
+    parser_scheduler_continue.add_argument('-r', '--resign', action='store', type=int, default=5, help='number of times a job is allowed to fail before it is removed from scheduling.')
+    parser_scheduler_continue.add_argument('-m', '--sendmail', action='store', type=str, help='a mail will be sent to this address when scheduling is paused or finished. Uses the "mail" command. ')
     # Layer 1: Scheduler Stop command
-    parser_scheduler_stop = subparsers_scheduler.add_parser('stop', help='stop gracefully after all busy worker have '
-                                                                         'finished and write remaining jobs to '
-                                                                         'continuation file')
+    parser_scheduler_stop = subparsers_scheduler.add_parser('stop', help='stop gracefully after all busy worker have finished and write remaining jobs to continuation file')
     assert isinstance(parser_scheduler_stop, argparse.ArgumentParser)
     parser_scheduler_stop.set_defaults(func=scheduler_stop)
     # Layer 0: Worker command
@@ -553,20 +506,16 @@ def startup():
     # Layer 1: Worker Start command
     parser_worker_start = subparsers_worker.add_parser('start', help='start Worker')
     assert isinstance(parser_worker_start, argparse.ArgumentParser)
-    parser_worker_start.add_argument('identifier', type=str, default='1',
-                                     help='unique identifier on this machine')
-    parser_worker_start.add_argument('-i', '--idletime', action='store', type=float, default=5,
-                                     help='timespan to sleep when there is nothing to to (job polling interval).')
+    parser_worker_start.add_argument('identifier', type=str, default='1', help='unique identifier on this machine')
+    parser_worker_start.add_argument('-i', '--idletime', action='store', type=float, default=5, help='timespan to sleep when there is nothing to to (job polling interval).')
     parser_worker_start.set_defaults(func=worker_start)
     # Layer 1: Worker Stop command
     parser_worker_stop = subparsers_worker.add_parser('stop', help='stop gracefully after current job')
     assert isinstance(parser_worker_stop, argparse.ArgumentParser)
-    parser_worker_stop.add_argument('identifier', type=str, default='1',
-                                    help='unique identifier on this machine')
+    parser_worker_stop.add_argument('identifier', type=str, default='1', help='unique identifier on this machine')
     parser_worker_stop.set_defaults(func=worker_stop)
     # Layer 1: Worker Stopall command
-    parser_worker_stopall = subparsers_worker.add_parser('stopall', help='stop all workers gracefully after their '
-                                                                         'current jobs')
+    parser_worker_stopall = subparsers_worker.add_parser('stopall', help='stop all workers gracefully after their current jobs')
     assert isinstance(parser_worker_stopall, argparse.ArgumentParser)
     parser_worker_stopall.set_defaults(func=worker_stopall)
     # Layer 0: Init command
@@ -584,8 +533,7 @@ def startup():
     # Layer 0: Help command
     parser_help = subparsers.add_parser('help', help='show help')
     assert isinstance(parser_help, argparse.ArgumentParser)
-    parser_help.add_argument('-w', '--width', type=int, default='80',
-                             help='line width of help text')
+    parser_help.add_argument('-w', '--width', type=int, default='80', help='line width of help text')
     parser_help.set_defaults(func=help)
 
     args = parser.parse_args()
