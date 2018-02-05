94d5b1d0c6a2b636c26e8fa7b4320f64367a84e2
Hendrik Fichtenberger <hendrik.fichtenberger@tu-dortmund.de>
2018-02-05 14:59:34 +0100

    license

--- fibs.py
+++ fibs.py
@@ -2,6 +2,21 @@
 
 """FIBS - a file-based scheduler"""
 
+# Copyright (C) 2016 Hendrik Fichtenberger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 try:
 	import argparse
 except (ImportError, NameError):
